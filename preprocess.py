f88d5c080cd1e1d221c72dee17a3e311cb033f55
Yvonne Chen <jc5349@columbia.edu>
2020-06-30 18:41:40 -0400

    Modified preprocessor
    Print error message when book language is not English.
    Lemmatize tokens.

--- preprocess.py
+++ preprocess.py
@@ -4,12 +4,20 @@ import sys
 import nltk
 import string
 import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 def str_preprocess(str):
     tokens = nltk.word_tokenize(str.lower())
     tokens = [re.sub(r'[^a-z]','',token) for token in tokens]
     tokens = [token for token in tokens if token]
-    return tokens
+
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+
+    lemmatizer = WordNetLemmatizer()
+    lemmas = [lemmatizer.lemmatize(token, pos = 'v') for token in filtered_tokens]
+    return lemmas
 
 with open(sys.argv[1], 'r') as book_file:
     book = book_file.read()
@@ -21,8 +29,14 @@ split_title = split_header[1].split('\n\nAuthor: ', 1)
 title = re.sub(r'\s+', ' ', split_title[0])
 split_author = split_title[1].split('\n\nRelease Date: ', 1)
 author = split_author[0]
-print(f'{title} by {author}')
-split_begin = split_author[1].split('\n\n*** START OF THIS PROJECT GUTENBERG EBOOK', 1)
+split_date = split_author[1].split('\n\nLanguage: ', 1)
+split_language = split_date[1].split('\n\nCharacter ', 1)
+if not (split_language[0] == 'English'):
+    print('Book language is not supported.')
+    sys.exit()
+split_begin = split_language[1].split('\n\n*** START OF THIS PROJECT GUTENBERG EBOOK', 1)
 split_end = split_begin[1].split('End of the Project Gutenberg EBook', 1)
+
 res = str_preprocess(split_end[0])
+print(f'{title} by {author}')
 print(res[:600])
