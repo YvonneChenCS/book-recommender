4b7a274b8d918735c15c1d12fab7c9e67e5a8e4c
Yvonne Chen <jc5349@columbia.edu>
2020-08-11 18:22:14 -0400

    Changed recommend.py into a importable module.

--- recommend.py
+++ recommend.py
@@ -17,19 +17,22 @@ def calculate_distances(bookid):
     eigens = pd.read_hdf(f'{current_dir}/eigens.h5').iloc[::-1].iloc[:100,0].values
     book = bookscores.loc[bookid]
     bookscores['dist'] = bookscores.apply(get_dist, axis=1)
-    return bookscores.sort_values('dist')
+    
+    scores = bookscores.sort_values('dist')
+    info = pd.read_hdf('bookinfo.h5')
+    scores = scores.join(info)
+    return scores
 
-parser = argparse.ArgumentParser(description='Recommend 5 closest books to [bookid].')
-parser.add_argument('bookid', metavar='bookid', help='bookid')
-argv = parser.parse_args()
-bookid = argv.bookid
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Recommend 5 closest books to [bookid].')
+    parser.add_argument('bookid', metavar='bookid', help='bookid')
+    argv = parser.parse_args()
+    bookid = argv.bookid
 
-try:
-    scores = calculate_distances(bookid)
-except KeyError:
-    print('book not found')
-    exit(1)
+    try:
+        scores = calculate_distances(bookid)
+    except KeyError:
+        print('book not found')
+        exit(1)
 
-info = pd.read_hdf('bookinfo.h5')
-scores = scores.join(info)
-print(scores[['dist', 'title', 'author']].head())
+    print(scores[['dist', 'title', 'author']].head())
