f4633e33c41291efe0aac2daedb406d8c54d20f8
Yvonne Chen <jc5349@columbia.edu>
2020-08-10 22:50:41 -0400

    Simple testing script for recommendation accuracy. Usage: ./recommend.py [bookid]

---
+++ recommend.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import argparse
+import pathlib
+import pandas as pd
+import numpy as np
+
+def calculate_distances(bookid):
+    def get_dist(row):
+        diff = np.subtract(row.values, book.values)
+        diff = np.square(diff)
+        wts = eigens
+        return np.sum(diff)
+
+    current_dir = pathlib.Path('recommend.py').parent.absolute()
+    bookscores = pd.read_hdf(f'{current_dir}/bookscores.h5').iloc[:,:100]
+    eigens = pd.read_hdf(f'{current_dir}/eigens.h5').iloc[::-1].iloc[:100,0].values
+    book = bookscores.loc[bookid]
+    bookscores['dist'] = bookscores.apply(get_dist, axis=1)
+    return bookscores.sort_values('dist')
+
+parser = argparse.ArgumentParser(description='Recommend 5 closest books to [bookid].')
+parser.add_argument('bookid', metavar='bookid', help='bookid')
+argv = parser.parse_args()
+bookid = argv.bookid
+
+try:
+    scores = calculate_distances(bookid)
+except KeyError:
+    print('book not found')
+    exit(1)
+
+info = pd.read_hdf('bookinfo.h5')
+scores = scores.join(info)
+print(scores[['dist', 'title', 'author']].head())
