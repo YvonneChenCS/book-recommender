ee4a3b9a2f0b08f79c36eeecf9233662a2b0144c
Yvonne Chen <jc5349@columbia.edu>
2020-08-16 23:41:30 -0400

    Added search route to backend

--- backend.py
+++ backend.py
@@ -1,11 +1,20 @@
 import recommend
+import search
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/recommend', methods=['POST'])
 def recommend_books():
     bookid = request.form['bookid']
     scores = recommend.calculate_distances(bookid)
     scores = scores[['title', 'author']].head()
     print(scores)
     return scores.to_json(orient='records')
+
+@app.route('/search', methods=['POST'])
+def search_books():
+    keyword = request.form['keyword']
+    id_dict = search.build_dict()
+    search_bookinfo = search.fuzzy_search(keyword, id_dict)
+    print(search_bookinfo)
+    return search_bookinfo.to_json(orient='records')
